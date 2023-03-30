7ccc7101620f3737ed3994a7fea43f9d7d7ab1f3
avishay-oz <avishaioz04@gmail.com>
2023-03-30 19:50:20 +0300

    save 🤗

--- Lab3/hello_jfx/src/main/java/org/example/PrimaryController.java
+++ Lab3/hello_jfx/src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,25 @@
+/**
+ * Sample Skeleton for 'primary.fxml' Controller Class
+ */
+
 package org.example;
 
-import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class PrimaryController {
 
+    @FXML // fx:id="helloBtn"
+    private Button helloBtn; // Value injected by FXMLLoader
+
+    @FXML // fx:id="helloTF"
+    private TextField helloTF; // Value injected by FXMLLoader
+
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    void sayHello(ActionEvent event) {
+        helloTF.setText("Hello World!");
     }
+
 }
